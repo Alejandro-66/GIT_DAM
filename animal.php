38cd91bad9a9dff8a25fb95ef9a7966c89bbb182
saul.henriquez@insbaixcamp.cat <saul.henriquez@insbaixcamp.cat>
2025-03-28 19:10:22 +0100

    Animal.php

---
+++ animal.php
@@ -0,0 +1,12 @@
+<?php
+
+class Animal{
+    private $nom;
+    private $edat;
+
+    public function__construct($nomP,$edatP) {
+        $this->nom = $nomP;
+        $this->edat = $edatP;
+    }
+}
+?>
